75969bb0982a80260b995e2be51990523a7c0fa3
rudinawang <rudnawang07@gmail.com>
2023-06-05 01:23:13 +0545

    SecondCommit 2080-02-22

--- api/app.js
+++ api/app.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const app = express();
 
+/* ROUTE HANDKERS */
+
+/* lIST ROUTES */
+
+/**
+ * GET /lists
+ * Purpose: Get all lists
+ */
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //we want to return an array at all the lists in the database
+});
+
+/**
+ * POST /lists
+ * Purpose: Create a list
+ */
+app.post("/", (req, res) => {
+  //we want to create a new list and return the new list document back to the user (which include the id)
+  // the list information (fields) will be passed in via the JSON request body
+});
+
+/**
+ * PATCH /lists/id
+ * Purpose: Update a specified list
+ */
+app.patch("/lists/:id", (req, res) => {
+  // we want to update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+});
+
+/**
+ * DELETE /lists/:id
+ * Purpose: Delete a list
+ */
+app.delete("/lists/:id", (req, res) => {
+  // we want to delete the specified list (document with id in the URL)
 });
 
 app.listen(3000, () => {
---
+++ api/db/mongoose.js
@@ -0,0 +1,3 @@
+// This file will handle connection logic to the MongoDB database
+
+const mongoose = require("mongoose");
