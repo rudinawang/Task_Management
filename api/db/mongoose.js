263936372052c5ca3c65d7448a162b9072db040d
rudinawang <rudnawang07@gmail.com>
2023-06-05 17:10:58 +0545

    ThirdCommit 2080-02-22

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -1,3 +1,24 @@
 // This file will handle connection logic to the MongoDB database
 
 const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost:27017/TaskManager", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongodb successfully :");
+  })
+  .catch((e) => {
+    console.log("Erroe while attempting to connect to MongoDB");
+    console.log(e);
+  });
+
+// To prevent description warnings (from MongoDB native driver)
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", false);
+
+module.exports = {
+  mongoose,
+};
